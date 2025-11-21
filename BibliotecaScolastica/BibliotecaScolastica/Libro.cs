b0921f710556872e83f0a8e9882e8e060839239e
Alessia Gentili <ALESSIA.GENTILI@local.ittcesena.it>
2025-11-21 08:37:47 +0100

    0.3: Aggiunto metodo readingTime. Implementato il costruttore

--- BibliotecaScolastica/BibliotecaScolastica/Libro.cs
+++ BibliotecaScolastica/BibliotecaScolastica/Libro.cs
@@ -9,15 +9,38 @@ namespace BibliotecaScolastica
 {
     internal class Libro
     {
-        public Libro()
+        private string nome;
+        private string autore;
+        private int numeroPagine;
+
+        public Libro(string nome, string autore, int numeroPagine)
         {
-            
+            this.nome = nome;
+            this.autore = autore;
+            this.numeroPagine = numeroPagine;
         }
 
         // Metodo per la restituzione dei valori degli attributi
         public override string ToString()
         {
-            return $"Nome del libro, Autore del libro";
+            return $"Nome del libro: {nome}, Autore del libro: {autore}, Numero pagine deli libro: {numeroPagine}";
+        }
+
+        public void readingTime ()
+        {
+            if (numeroPagine < 100)
+            {
+                Console.WriteLine("Il tempo medio per leggere il libro sarà di 1h");
+
+            }
+            else if (numeroPagine > 100 && numeroPagine < 200)
+            {
+                Console.WriteLine("Il tempo medio per leggere il libro sarà di 2h");
+            }
+            else
+            {
+                Console.WriteLine("Il tempo megio per leggere il libro sarà di 3h");
+            }
         }
     }
 
