de967d1d34033f592a30e9799e93f0f727d79adf
Alessia Gentili <ALESSIA.GENTILI@local.ittcesena.it>
2025-11-21 09:31:00 +0100

    0.11: Implementata la scleta per l'utente di poter scegelire cosa fare dopo aver aggiunto il libro

--- BibliotecaScolastica/BibliotecaScolastica/Program.cs
+++ BibliotecaScolastica/BibliotecaScolastica/Program.cs
@@ -10,8 +10,16 @@ namespace BibliotecaScolastica
     {
         static void Main(string[] args)
         {
+            
+
             Console.WriteLine("Benvenuto");
 
+            Console.WriteLine("Inserisci il nome della biblioteca a cui vuoi accedere");
+            string nomeBiblioteca = Console.ReadLine();
+
+            int orarioApertura = 9;
+
+            int orarioChiusura = 21;
             Console.WriteLine("\nInserisci il nome del libro:");
             string nome = Console.ReadLine();
 
@@ -21,12 +29,37 @@ namespace BibliotecaScolastica
             Console.WriteLine("\nInserisci il numero di pagine del libro:");
             string numeroPagine = Console.ReadLine();
 
-            Console.WriteLine("Vuoi confermare la creazione del libro?");
+            Console.WriteLine("\n Vuoi confermare la creazione del libro?");
             string risposta = Console.ReadLine();
+            
+            Libro libro = new Libro(nome, autore, numeroPagine);
+            Biblioteca biblioteca = new Biblioteca(nomeBiblioteca, orarioApertura, orarioChiusura);
             
             if (risposta == "si")
             {
-                Libro lib = new Libro(nome, autore, numeroPagine);
+                libro.ToString();
+            }
+            
+            
+            Console.WriteLine("Diita 1 per accedere alle informazioni riguardo alla biblioteca");
+            Console.WriteLine("Digita 2 per visualizare la media del tempo di lettura");
+            Console.WriteLine("Digita 3 se vuoi sucire dal programma definitivamente");
+            string scelta = Console.ReadLine();
+
+            switch (scelta)
+            {
+                case "1":
+                    libro.readingTime();
+                break;
+
+                case "2":
+                    // Collegamento con la biblioteca per visualizare il le informazioni relative alla biblioteca
+                break;
+
+                case "3":
+                    Console.WriteLine("Grazie per aver usato il nostro servizio online");
+                return;
+
             }
         }
     }
