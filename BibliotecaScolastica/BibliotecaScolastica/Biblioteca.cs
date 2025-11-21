5b0984d21132067ff8e91f5d50eaeb712a9a0fc0
Alessia Gentili <ALESSIA.GENTILI@local.ittcesena.it>
2025-11-21 08:46:47 +0100

    0.4: Implementato il costruttore

---
+++ BibliotecaScolastica/BibliotecaScolastica/Biblioteca.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaScolastica
+{
+    internal class Biblioteca
+    {
+        private string nomeBiblioteca;
+        private int orarioApertura;
+        private int orarioChiusura;
+        public Biblioteca(string nomeBiblioteca, int orarioApertura, int orarioChiusura) 
+        { 
+            this.nomeBiblioteca = nomeBiblioteca;
+            this.orarioChiusura = orarioChiusura;
+            this.orarioApertura = orarioApertura;
+        }
+    }
+}
