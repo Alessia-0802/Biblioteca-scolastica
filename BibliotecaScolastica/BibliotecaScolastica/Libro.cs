95b27896faa5abd50c7e10dcae2d18403a390905
Alessia Gentili <ALESSIA.GENTILI@local.ittcesena.it>
2025-11-21 08:30:56 +0100

    0.2: Creato il metodo ToString

---
+++ BibliotecaScolastica/BibliotecaScolastica/Libro.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotecaScolastica
+{
+    internal class Libro
+    {
+        public Libro()
+        {
+            
+        }
+
+        // Metodo per la restituzione dei valori degli attributi
+        public override string ToString()
+        {
+            return $"Nome del libro, Autore del libro";
+        }
+    }
+
+    
+    
+}
